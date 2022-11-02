8102bac106f4d8d42f6f800039702bbc22a52c57
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-02 21:55:04 +0100

    Updating Upper App bar

--- lib/widgets/home_page/upper_app_bar.dart
+++ lib/widgets/home_page/upper_app_bar.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
 
+import '../constants.dart';
+
 class Upper_Nav extends StatelessWidget {
   const Upper_Nav({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'Styles'.toUpperCase(),
+          style: const TextStyle(color: primaryColor),
+        ),
+        Text(
+          'Design'.toUpperCase(),
+          style: const TextStyle(color: primaryColor),
+        ),
+        Text(
+          'Technology'.toUpperCase(),
+          style: const TextStyle(color: primaryColor),
+        ),
+        Image.asset('assets/logo/official_logo.png'),
+        Text(
+          'Performance'.toUpperCase(),
+          style: const TextStyle(color: primaryColor),
+        ),
+        Text(
+          'Gallery'.toUpperCase(),
+          style: const TextStyle(color: primaryColor),
+        ),
+        Text(
+          'Safety'.toUpperCase(),
+          style: const TextStyle(color: primaryColor),
+        ),
+        Text(
+          'Offer'.toUpperCase(),
+          style: const TextStyle(color: primaryColor),
+        ),
+      ],
+    ));
   }
 }
